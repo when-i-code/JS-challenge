8da1c89937641fdf4c03388787c5df6bf42a3c56
Adriana <adrianasavastre@gmail.com>
2022-07-05 13:49:22 +0300

    day2 JS updates

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
-// console.log('It works');
+// // console.log('It works');
 
-/* function hoursToSec (num) {
-    return num*3600;
-};
+// /* function hoursToSec (num) {
+//     return num*3600;
+// };
 
-console.log(hoursToSec(24)); */
+// console.log(hoursToSec(24)); */
 
-/* function calcPermimeter(length, witdth) {
-    return 2*length+2*witdth;
+// /* function calcPermimeter(length, witdth) {
+//     return 2*length+2*witdth;
+// }
+// console.log(calcPermimeter(20,10))*/
+
+// /* function plusFrontend(name) {
+//     return name + "Frontend";
+// }
+// console.log(plusFrontend('Apple')); */
+
+// // function sum(a){
+// //     return a===0;
+// // }
+
+// // console.log(sum(-1));
+
+// let cash = 20
+// let price = 40
+// let isStoreOpen = true
+// cash>=price && isStoreOpen ? console.log('receipt printing') : console.log('cannot print receipt')
+
+/* let count = 1
+while (count <= 100) {
+    console.log('number is: ' + count)
+    count = count + 1
+
+} */
+
+let string = "Frontend Simplified"
+//let stringLength = string.length
+
+for(i=0; i< string.length; i++) {
+    console.log(`This is the number ${i}  of the string, and its correspinding letter is: ${string[i]}`)
 }
-console.log(calcPermimeter(20,10))*/
-
-/* function plusFrontend(name) {
-    return name + "Frontend";
-}
-console.log(plusFrontend('Apple')); */
-
-function sum(a){
-    return a===0;
-}
-
-console.log(sum(-1));
