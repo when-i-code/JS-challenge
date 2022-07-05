44f7f7a0e9417b8a5a340cbe796d733e4a8de28c
Adriana <adrianasavastre@gmail.com>
2022-07-05 12:08:11 +0300

    day2 JS

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // console.log('It works');
 
+/* function hoursToSec (num) {
+    return num*3600;
+};
+
+console.log(hoursToSec(24)); */
+
+/* function calcPermimeter(length, witdth) {
+    return 2*length+2*witdth;
+}
+console.log(calcPermimeter(20,10))*/
+
+/* function plusFrontend(name) {
+    return name + "Frontend";
+}
+console.log(plusFrontend('Apple')); */
+
+function sum(a){
+    return a===0;
+}
+
+console.log(sum(-1));
